c7e9d7b0b591d90b150090743086f7f7578c6b81
vanisha-sirpi <vanisha.sirpi@gmail.com>
2026-04-02 16:33:28 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ tasks = []
 
 while True:
     print("\n--- TO DO LIST ---")
-    print("1. View Tasks")
+    print("1. Available Tasks")
     print("2. Add Task")
     print("3. Delete Task")
     print("4. Exit")
